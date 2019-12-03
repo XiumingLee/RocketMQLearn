00aa149215fd01841fbed0ff07b2e4835fdff074
Xiuming Lee <lxmcoder@gmail.com>
2019-12-03 18:56:32 +0800

    环境测试...

---
+++ rocketmq-base/src/main/java/cn/xiuminglee/consumer/Consumer.java
@@ -0,0 +1,44 @@
+package cn.xiuminglee.consumer;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
+
+import java.util.List;
+
+/**
+ * @Author Xiuming Lee
+ * @Description 消息消费者
+ */
+public class Consumer {
+    public static void main(String[] args) throws MQClientException {
+        //1.创建消费者Consumer，制定消费者组名
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("group1");
+        //2.指定NameServer地址
+        consumer.setNamesrvAddr("localcentos7:9876");
+        //3.订阅主题Topic和Tag
+        consumer.subscribe("base", "*");
+
+        //设定消费模式：负载均衡|广播模式
+        consumer.setMessageModel(MessageModel.CLUSTERING);
+
+        //4.设置回调函数，处理消息
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+
+            //接受消息内容
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                for (MessageExt msg : msgs) {
+                    System.out.println("consumeThread=" + Thread.currentThread().getName() + "," + new String(msg.getBody()));
+                }
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        //5.启动消费者consumer
+        consumer.start();
+    }
+}
